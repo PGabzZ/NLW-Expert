80ba70abd6dd719330ae2fc7100a6795686133c9
PGabzZ <gabriels.picolo@gmail.com>
2024-02-05 23:39:02 -0300

    Day 1

---
+++ src/app.tsx
@@ -0,0 +1,29 @@
+import logo from  "./assets/Logo-nlw-expert.svg"
+import { NoteCard } from "./components/note-card"
+
+export function App() {
+  
+return (
+  <div className="mx-auto max-w-6xl space-y-6">
+    <img src={logo} alt="nlw-expert"/>
+    <form className="w-full">
+      <input type="text" 
+      placeholder="Busque por suas notas.."
+      className="w-full bg-transparent text-3xl font-semibold tracking-tight outline-none placeholder:text-slate-500"/>
+    </form>
+
+    <div className="h-px bg-slate-700"/>
+
+    <div className="grid grid-cols-3 gap-6 auto-rows-[250px]">
+      
+    <NoteCard/>
+    <NoteCard/>
+    <NoteCard/>
+    <NoteCard/>
+    <NoteCard/>
+    
+    </div>
+  </div>
+)
+}
+
